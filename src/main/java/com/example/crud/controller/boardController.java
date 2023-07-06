54539a5339c1ee2e3724e2dc1e563fa7bb6cc382
NAMUORI <rladbtjr0227@namuori.net>
2023-07-06 10:35:36 +0900

    [feat]: [게시글 작성자 닉네임 포함 출력 추가]

--- src/main/java/com/example/crud/controller/boardController.java
+++ src/main/java/com/example/crud/controller/boardController.java
@@ -1,12 +1,38 @@
 package com.example.crud.controller;
 
+import com.example.crud.dto.Board;
+import com.example.crud.repository.UserRepository;
+import com.example.crud.repository.boardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class boardController {
+
+    @Autowired
+    private boardRepository BoardRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping({"/board", "/board/list"})
-    public String board() {
+    public String board(Model model){
+        List<Board> boards = BoardRepository.findAll();
+
+        List<String> nicknames = new ArrayList<>();  // 작성자 닉네임을 저장할 리스트
+        for (Board board : boards) {
+            String nickname = board.getAuthor().getNickname();  // 작성자의 닉네임 정보를 가져옴
+            nicknames.add(nickname);  // 작성자의 닉네임을 리스트에 추가
+        }
+
+        model.addAttribute("boards", boards);
+        model.addAttribute("nicknames", nicknames);  // 작성자 닉네임 리스트를 모델에 추가
+
         return "board/list";
     }
 }
