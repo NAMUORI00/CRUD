50d9424e6ddda12862636e10e8815977074c9fd0
NAMUORI <rladbtjr0227@namuori.net>
2023-07-06 11:41:30 +0900

    [feat]: [게시글 작성, 수정 로직 추가, 작성일, 업데이트일 자동로직 추가]

--- src/main/java/com/example/crud/controller/boardController.java
+++ src/main/java/com/example/crud/controller/boardController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,5 +38,23 @@ public class boardController {
 
         return "board/list";
     }
+
+    @GetMapping({"/form"})
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if (id == null){
+            model.addAttribute("board", new Board());
+        }else {
+            Board board = BoardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+        return "board/form";
+    }
+
+    @PostMapping({"/form"})
+    public String postForm(@ModelAttribute Board board) {
+        BoardRepository.save(board);
+        return "redirect:/board/list";
+    }
+
 }
 
--- src/main/java/com/example/crud/dto/Board.java
+++ src/main/java/com/example/crud/dto/Board.java
@@ -3,6 +3,7 @@ package com.example.crud.dto;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -14,8 +15,20 @@ public class Board {
     private String title;
     private String content;
     private long author_id;
-    private String created_at;
-    private String updated_at;
+    private LocalDateTime created_at; // 생성일시
+    private LocalDateTime updated_at; // 수정일시
+
+    // 엔티티 저장 전에 호출되는 메서드
+    @PrePersist
+    protected void onCreate() {
+        created_at = LocalDateTime.now();
+    }
+
+    // 엔티티 업데이트 전에 호출되는 메서드
+    @PreUpdate
+    protected void onUpdate() {
+        updated_at = LocalDateTime.now();
+    }
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "author_id", referencedColumnName = "id", insertable = false, updatable = false)
@@ -23,4 +36,6 @@ public class Board {
 
     @OneToMany(mappedBy = "board")
     private List<Comments> comments;
+
+
 }
